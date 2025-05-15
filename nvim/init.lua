8cf058f4abe00e269e7f1e7945349e3e172ad20c
mirrrrrrrrrrrr <mirrrrrr@yahoo.com>
2025-05-15 11:16:11 +0500

    config files updated

--- nvim/init.lua
+++ nvim/init.lua
@@ -8,7 +8,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { 'Failed to clone lazy.nvim:\n', 'ErrorMsg' },
-      { out,                            'WarningMsg' },
+      { out, 'WarningMsg' },
       { '\nPress any key to exit...' },
     }, true, {})
     vim.fn.getchar()
@@ -30,4 +30,5 @@ require('lazy').setup {
   require 'plugins.alpha',
   require 'plugins.indent-blankline',
   require 'plugins.misc',
+  require 'plugins.gitsigns',
 }
--- nvim/lua/plugins/non-ls.lua
+++ nvim/lua/plugins/non-ls.lua
@@ -6,16 +6,16 @@ return {
   },
   config = function()
     local null_ls = require 'null-ls'
-    local formatting = null_ls.builtins.formatting   -- to setup formatters
+    local formatting = null_ls.builtins.formatting -- to setup formatters
     local diagnostics = null_ls.builtins.diagnostics -- to setup linters
 
     -- Formatters & linters for mason to install
     require('mason-null-ls').setup {
       ensure_installed = {
-        'prettier',  -- ts/js formatter
-        'stylua',    -- lua formatter
-        'eslint_d',  -- ts/js linter
-        'shfmt',     -- Shell formatter
+        'prettier', -- ts/js formatter
+        'stylua', -- lua formatter
+        'eslint_d', -- ts/js linter
+        'shfmt', -- Shell formatter
         'checkmake', -- linter for Makefiles
         --        'ruff', -- Python linter and formatter
       },
@@ -24,7 +24,7 @@ return {
 
     local sources = {
       diagnostics.checkmake,
-      formatting.prettier.with { filetypes = { 'html', 'json', 'yaml', 'markdown', 'typescript' } },
+      formatting.prettier.with { filetypes = { 'html', 'json', 'yaml', 'markdown', 'typescript', 'javascript' } },
       formatting.stylua,
       formatting.shfmt.with { args = { '-i', '4' } },
       formatting.terraform_fmt,
