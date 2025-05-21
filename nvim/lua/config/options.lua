f3ed58b2fc6b80bc408ebe91003547dab6d1bdd9
mirrrrrrrrrrrr <mirrrrrr@yahoo.com>
2025-05-22 00:44:11 +0500

    add new nvim config

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -4,6 +4,7 @@ vim.opt.shiftwidth = 4 -- Amount to indent with << and >>
 vim.opt.tabstop = 4 -- How many spaces are shown per Tab
 vim.opt.softtabstop = 4 -- How many spaces are applied when pressing Tab
 vim.opt.clipboard = "unnamedplus"
+vim.opt.wrap = false
 
 vim.opt.smarttab = true
 vim.opt.smartindent = true
